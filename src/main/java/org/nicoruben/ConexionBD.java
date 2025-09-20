c4fe79f8f49b3d845384bced7053df82a752ebe9
ruben <ruben.gallardo@inslapineda.cat>
2025-09-20 15:45:17 +0200

    Migracion a alwaysdata.com - BBDD

--- src/main/java/org/nicoruben/ConexionBD.java
+++ src/main/java/org/nicoruben/ConexionBD.java
@@ -5,10 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
+
+    // alwaysdata.com - DataBase MySQL Free
+    // Base de Datos: rubengmpineda_gym
+    // Usuario: 431396
+    // Password: LaPineda2526
+
     // URL de conexión: jdbc:mysql://<host>:<puerto>/<nombre_base>
-    private static final String URL = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7799378";
-    private static final String USUARIO = "sql7799378";
-    private static final String PASSWORD = "FnXykGLe7T";
+    private static final String URL = "jdbc:mysql://mysql-rubengmpineda.alwaysdata.net:3306/rubengmpineda_gym?useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String USUARIO = "431396_gym";
+    private static final String PASSWORD = "LaPineda2526";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, PASSWORD);
